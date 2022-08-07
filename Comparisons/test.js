8dc67f195b06e1ca486ae996e4c66aab46a6fb21
umitkezer <110773926+umitkezer@users.noreply.github.com>
2022-08-07 21:47:29 +0300

    Update test.js

--- Comparisons/test.js
+++ Comparisons/test.js
@@ -1,21 +1,46 @@
-console.log(Boolean(1));
-console.log(Boolean(0));
-console.log(Boolean('hello'));
-console.log(Boolean(''));
+// console.log(Boolean(1));
+// console.log(Boolean(0));
+// console.log(Boolean('hello'));
+// console.log(Boolean(''));
 
-console.log(Boolean('0'))
-const x = 1; y= 3 ;
-
-
-console.log(y-x);
-
-const s = 'my' + 'string';
-console.log(s);
+// console.log(Boolean('0'))
+// const x = 1; y= 3 ;
 
 
-console.log('6' + '2');             
+// console.log(y-x);
+
+// const s = 'my' + 'string';
+// console.log(s);
 
 
-const ab = 2 *2 +1;
-console.log
+// console.log('6' + '2');             
 
+
+// const ab = 2 *2 +1;
+// console.log
+
+
+// let q = 1 ; 
+//  let w = 2 ; 
+
+//  let c = 5 - (q =w  + 1 ); 
+
+//  console.log(q);
+//  console.log(c);
+// // 
+
+// let counter = 2 ; 
+// counter++;
+// console.log(counter);
+
+
+// let counter1 = 1 ; 
+// let a = ++counter1;
+// console.log(counter1);
+
+
+let value = 1;
+
+console.log(2 * value++);
+
+console.log(2 * ++value ); 
