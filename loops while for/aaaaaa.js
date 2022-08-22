e34bb10c22a7c73e916145d832019b4e1f7ea449
umitkezer <110773926+umitkezer@users.noreply.github.com>
2022-08-22 14:07:15 +0300

    lesson repetition

---
+++ loops while for/aaaaaa.js
@@ -0,0 +1,54 @@
+/*tekrarlayan işlemlerdew bir koşul oluşturarak 
+koşula bağlı olarak bir koda  yazılabilir.*/
+ 
+
+for (let i = 0 ; i < 10; i++ ){
+    //kod
+    // console.log(i);
+    console.log("hello");
+}
+
+/*
+döngü içine yazıla koşul doğru ise yazdılır.
+koşul false olur ise konsola işletilmez 
+i++ yazılmaz ise i her defasında 10 dan küçük olacağı için sürekli yazılır ama İ++ yazıldığında 1 arttıracağı için 9 dan son bulur
+illaki i değerini yazdırmak zorunda değiliz "" tınaklar içinde "merhaba" değer değişebilir yazabiliriz 
+
+ */
+
+let total = 0 ;
+
+for(let a = 0 ; a < 10 ; a++){
+    total += a;
+}
+console.log(total);
+
+
+
+
+/*
+diğerşimki 1 den 10 kadar ki değerlerin toplamını bulmak için 
+total değişkeni tanımlayıp değer atayalım 
+her gelen a değerine (total +0 a) şeklinde toplaconsole.log(total);
+
+kod bloklarından çıktık tan sonra yazarsak bize toplam değeri verir 
+let total = 0 ;
+
+for(let a = 0 ; a < 10 ; a++){
+    total += a;
+}
+console.log(total);
+
+
+
+kod bloklarınınj içine yazdığımızda ,
+let total = 0 ;
+
+for(let a = 0 ; a < 10 ; a++){
+    total += a;
+    console.log(total);
+}
+
+
+*/
+
--- loops while for/while.js
+++ loops while for/while.js
@@ -1,45 +1,35 @@
-// Çoğu zaman aynı bir sıra ile tekrar yapma ihtiyacı duyulabilirsiniz.
-// Örneğin bir listede bulunan ürünlerin sıra ile çıktısını almak. Veya aynı kodu 1-10’a kadar olan sayılar için çalıştırmak.
-// Döngü aynı kod parçacığının birden fazla defa çalıştırılmasına denir.
+/*
+-aynı bir sıra ile tekrar yapma ihtiyacı duyulabilir 
+-döngü bir kod parçacığının birden fazla defa çalıştırlımasına denir.
+*/
 
 
 
-// while (koşul) {
-    // kod
-    // veya döngünün gövdesi
-  // }   // gibi bir yazıma sahiptir.
+// WHİLE döngüsü
+/*
+while(koşul){
+  kod
+  veye dögünün gövdesi
+}
 
-//   koşul doğru iken(while), döngü gövdesinde bulunan kod çalıştırılır.
+koşul doğru iken(while) döngü gövdesinde bulunan kod çalışır.
+örneğin aşağıdaki kod i<3 iken çalışır.
+*/
+
 
 let i = 0;
-while (i < 3) { // önce 0, sonra 1, sonra 2
-console.log(i);  /// 0 ,1 ,2
-  i++;
-}
-
-/*
-Döngünün gövdesinde bulunan kodun her çalışmasına tekerrür(iteration) denir.
- Yukarıdaki örnekte gövde 3 defa tekerrür etmiştir.
- Eğer i++ gibi bir kod olmasaydı, teoride kod sonsuza kadar devam ederdi.
- Pratikte ise tarayıcınız bu kodun uzun süre çalışmasını engeller, sunucu tabanlı JavaScript yazdığınızda ise bu işlem durdurulur
- Sadece karşılaştırma değil, bir ifade veya değişken koşul olabilir.
- While döngüsü tarafından alınan tüm ifadeler boolean’a dönüştürülür.
+while (i<3){
+  console.log(i);
+  i++
+}  // 0  1   2 
 
 
+/*  döngünü gövdesinde bulumam kodun her çalımasına tekerrür(interation) denir
+yukardaki gövdede 3 defa tekerrür etmiştir
 
-
- Örneğin, while(i != 0 ) while(i)'de olabilir. */
-
-let a  = 3;
-while (a) {  // i 0 olduğunda koşul `yanlış` olur ve döngü biter.
-  console.log(a);  // 3 ,2 ,1 
-  a--;
-}
-
-
-
-/* !!!! Tek satır gövde varsa süslü paranteze gerek kalmaz.
-Eğer döngü gövdesi tek satır ise süslü parantezi yazmayabilirsiniz. {..}: */
-
-let x = 3;
-while(x)console.log(x--); //3, 2, 1
+eğer i++ gibi bir kod olmasaydı teoride kod sonsuza kadar devam ederdi 
+pratikte ise tarayıcınız bu kodun uzun süre çalışmasını engeller sunucu tabanlı js yazdığımızda ise bu işlem durdurulur.
+sadece karşılatırma değil bir ifade veya değişken koşul olabilir 
+ehile döngüsü tarafından alınan tüm ifadeler boolean a dönüştürülür 
+örn;
+while (i !=0) while i de olabilir.*/
