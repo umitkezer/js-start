4d3ba41287839be708d7ede504ce078c4dde1466
umitkezer <110773926+umitkezer@users.noreply.github.com>
2022-09-12 15:14:40 +0300

    Update aaa.js

--- section_1/aaa.js
+++ section_1/aaa.js
@@ -1,18 +1,18 @@
-
-
 const isHuman = true;
 
 const age = 234;
 
-const surname = 'kezer';
+const surname = "kezer";
 
 console.log(typeof surname);
 
-const dividingBy = '5';
+const dividingBy = "5";
 
-const dividing = '10'
+const dividing = "10";
 
 const result = dividing / dividingBy;
 
 console.log(result);
 console.log(typeof result);
+
+console.log(typeof "elsnur");
